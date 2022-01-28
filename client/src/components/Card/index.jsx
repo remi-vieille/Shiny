8c6bd7d8e5558f0e731e82d06d654c60b5c0f34b
remi-vieille <remivieille13@gmail.com>
2022-01-29 00:52:37 +0100

    fetch api data added for survey and freelances pages

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -1,25 +1,38 @@
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import DefaultPicture from '../../assets/profile.png'
 import colors from '../../utils/styles/colors'
+import DefaultPicture from '../../assets/profile.png'
+
 const CardLabel = styled.span`
   color: #5843e4;
   font-size: 22px;
-  font-weight: bold;
+  font-weight: normal;
+  padding-left: 15px;
 `
+
+const CardTitle = styled.span`
+  color: black;
+  font-size: 22px;
+  font-weight: normal;
+  align-self: center;
+`
+
 const CardImage = styled.img`
-  height: 80px;
-  width: 80px;
+  height: 150px;
+  width: 150px;
+  align-self: center;
   border-radius: 50%;
 `
 
 const CardWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
   padding: 15px;
   background-color: ${colors.backgroundLight};
   border-radius: 30px;
-  width: 350px;
+  width: 300px;
+  height: 300px;
   transition: 200ms;
   &:hover {
     cursor: pointer;
@@ -31,8 +44,8 @@ function Card({ label, title, picture }) {
   return (
     <CardWrapper>
       <CardLabel>{label}</CardLabel>
-      <CardImage src={picture} alt="freelance" height={80} width={80} />
-      <span>{title}</span>
+      <CardImage src={picture} alt="freelance" />
+      <CardTitle>{title}</CardTitle>
     </CardWrapper>
   )
 }
@@ -48,4 +61,5 @@ Card.defaultProps = {
   title: '',
   picture: DefaultPicture,
 }
+
 export default Card
