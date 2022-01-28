33c988d181f50fcac56277c47606b0ad8afcffc8
remi-vieille <remivieille13@gmail.com>
2022-01-28 17:50:25 +0100

    styled component added

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Results from './pages/Results'
@@ -8,9 +9,16 @@ import Freelances from './pages/Freelances'
 import Header from './components/Header'
 import Error from './components/Error'
 
+const GlobalStyle = createGlobalStyle`
+    div {
+        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+`
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
@@ -21,5 +29,5 @@ ReactDOM.render(
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 )
