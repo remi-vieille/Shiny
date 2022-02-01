90cfbe734fa55c7e93165829f13c8b63fc724287
remi-vieille <remivieille13@gmail.com>
2022-02-01 21:15:06 +0100

    add component test for Footer and Card

---
+++ client/src/pages/Home/index.test.js
@@ -0,0 +1,23 @@
+import { MemoryRouter } from 'react-router-dom'
+import { render, screen } from '@testing-library/react'
+import Home from './'
+import { ThemeProvider } from '../../utils/context'
+
+describe('The Home component', () => {
+  it('should render title', () => {
+    render(
+      <MemoryRouter>
+        <ThemeProvider>
+          <Home />
+        </ThemeProvider>
+      </MemoryRouter>
+    )
+    expect(
+      screen.getByRole('heading', {
+        level: 2,
+        text:
+          "Repérez vos besoins, on s'occupe du reste, avec les meilleurs talents",
+      })
+    ).toBeTruthy()
+  })
+})
